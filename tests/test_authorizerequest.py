19d6da7d2901983ea90fa3b8e9ff891239d51e13
Marcel van den Dungen <marcel@van-den-dungen.com>
2020-04-24 23:16:45 -0700

    Added docstrings and unit tests

--- tests/test_authorizerequest.py
+++ tests/test_authorizerequest.py
@@ -4,15 +4,25 @@ from provider.model.authorize_request import AuthorizeRequest, AuthorizeRequestE
 
 
 def test_validate_scopes_with_valid_scopes():
+    """
+      GIVEN: Client configured with given scope
+      WHEN: AuthorizationRequest requesting same scope
+      THEN: validate_scope returns requested scope
+    """
     client = {
         'scope': 'read write'
     }
     ar = AuthorizeRequest(scope='read write')
-    ar.validate_scopes(client)
-    assert ar.scope == 'read write'
+    scopes = ar.validate_scopes(client)
+    assert scopes == 'read write'
 
 
 def test_validate_scopes_with_invalid_scopes():
+    """
+      GIVEN: Client configured with given scope
+      WHEN: AuthorizationRequest requesting different scope
+      THEN: validate_scope raises a AuthorizeRequestError
+    """
     client = {
         'scope': 'openid'
     }
@@ -22,9 +32,45 @@ def test_validate_scopes_with_invalid_scopes():
 
 
 def test_validate_scopes_with_no_scopes():
+    """
+      GIVEN: Client configured with given scope
+      WHEN: AuthorizationRequest with no specified scope
+      THEN: validate_scope returns scope configured in client
+    """
     client = {
         'scope': 'read write'
     }
     ar = AuthorizeRequest()
     scopes = ar.validate_scopes(client)
     assert scopes == 'read write'
+
+
+def test_validate_pkce_raises_error_when_code_challenge_missing():
+    """
+      GIVEN: Public client configured
+      WHEN: AuthorizationRequest with code_challenge attribute missing
+      THEN: validate_pkce raises a AuthorizeRequestError
+    """
+    client = {
+        'scope': 'read write',
+        'token_endpoint_auth_method': 'None'
+    }
+    with pytest.raises(AuthorizeRequestError):
+        ar = AuthorizeRequest()
+        ar.validate_pkce(client)
+
+
+def test_validate_pkce_raises_error_when_code_challenge_method_incorrect():
+    """
+      GIVEN: Public client configured
+      WHEN: AuthorizationRequest with unsupported code_challenge_method attribute
+      THEN: validate_pkce raises a AuthorizeRequestError
+    """
+    client = {
+        'scope': 'read write',
+        'token_endpoint_auth_method': 'None',
+        'code_challenge_method': 'plain'
+    }
+    with pytest.raises(AuthorizeRequestError):
+        ar = AuthorizeRequest()
+        ar.validate_pkce(client)
