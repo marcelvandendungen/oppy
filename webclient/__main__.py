eb6130edf53707c3289a5d4046f0d252aae8d358
Marcel van den Dungen <marcel@van-den-dungen.com>
2020-06-01 20:46:58 -0700

    Added argument parser to webclient

--- webclient/__main__.py
+++ webclient/__main__.py
@@ -1,6 +1,13 @@
+import argparse
 import sys
 
 from webclient import app
 
+
+parser = argparse.ArgumentParser(description='Flask commandline options')
+parser.add_argument('--config', type=str, default='webclient/config.yml')
+args = parser.parse_args()
+
+
 if __name__ == "__main__":
-    sys.exit(app.main())
+    sys.exit(app.main(args.config))
--- webclient/app.py
+++ webclient/app.py
@@ -14,20 +14,12 @@ from flask import Flask, request, redirect, make_response, render_template
 app = Flask(__name__)
 
 
-config = init_config('webclient/config.yml')
-logger = init_logging(__name__)
+logger = None
 scopes = {}
+client = None
 
 
 _, public_key = read_keys("./private.pem", "./public.pem")
-
-JKWS_ENDPOINT = config['endpoints']['issuer'] + config['endpoints']['jwks']
-TOKEN_ENDPOINT = config['endpoints']['issuer'] + config['endpoints']['token']
-AUTHORIZE_ENDPOINT = config['endpoints']['issuer'] + config['endpoints']['authorize']
-REGISTRATION_ENDPOINT = config['endpoints']['issuer'] + config['endpoints']['registration']
-RESOURCE_ENDPOINT = config['endpoints']['resource_server'] + config['endpoints']['resource']
-USERINFO_ENDPOINT = config['endpoints']['issuer'] + config['endpoints']['userinfo']
-POST_LOGOUT_URI = config['endpoints']['webclient'] + config['endpoints']['post_logout']
 
 
 class TokenError(Exception):
@@ -68,7 +60,7 @@ def get_public_key(url):
 
 def register_client(config):
 
-    url = REGISTRATION_ENDPOINT
+    url = app.config.get('REGISTRATION_ENDPOINT')
     headers = {
         'Content-Type': 'application/json'
     }
@@ -78,28 +70,11 @@ def register_client(config):
         "name": "confidential_client",
         "scope": "read write openid profile email roles",
         "frontchannel_logout_uri": "https://localhost:5001/logout",
-        "post_logout_redirect_uris": [POST_LOGOUT_URI]
+        "post_logout_redirect_uris": [app.config.get('POST_LOGOUT_URI')]
     }
 
     response = requests.post(url, headers=headers, json=payload, verify=False)
     return response.json()
-
-
-public_key = get_public_key(JKWS_ENDPOINT)
-# read client id and secret from environment
-client_id = os.environ.get('CLIENT_ID')
-if client_id:
-    client_secret = os.environ['CLIENT_SECRET']
-    redirect_uri = os.environ['REDIRECT_URI']
-else:
-    # if not set, register client and use it's id and secret
-    client = register_client(config)
-    client_id = client['client_id']
-    client_secret = client['client_secret']
-    redirect_uri = client['redirect_uris'][0]
-
-
-logger.info(f'Registered client with client_id: {client_id}')
 
 
 @app.route("/")
@@ -117,11 +92,11 @@ def index():
     try:
         if id_token:
             logger.info('id_token found: ' + str(id_token))
-            id_claims = get_token_claims(id_token, client_id)
+            id_claims = get_token_claims(id_token, client['client_id'], public_key)
 
             access_token = tokencache.get(state, 'openid', 'access_token')
-            response = requests.get(USERINFO_ENDPOINT, headers={'Authorization': 'Bearer ' + access_token},
-                                    verify=False)
+            response = requests.get(app.config.get('USERINFO_ENDPOINT'),
+                                    headers={'Authorization': 'Bearer ' + access_token}, verify=False)
             if response.status_code == 200:
                 userinfo_claims = response.json()
         else:
@@ -135,11 +110,12 @@ def index():
     try:
         if access_token:
             logger.info('access token found: ' + access_token)
-            response = requests.get(RESOURCE_ENDPOINT, headers={'Authorization': 'Bearer ' + access_token},
-                                    verify=False)
+            response = requests.get(app.config.get('RESOURCE_ENDPOINT'), 
+                                    headers={'Authorization': 'Bearer ' + access_token}, verify=False)
             if response.status_code == 200:
                 return render_template('index.html', token=response.json(), userinfo=userinfo_claims,
-                                       name=id_claims['name'], post_logout_uri=POST_LOGOUT_URI, state=str(uuid.uuid4()))
+                                       name=id_claims['name'], post_logout_uri=app.config.get('POST_LOGOUT_URI'),
+                                       state=str(uuid.uuid4()))
             elif response.status_code == 401:
                 logger.info("Access token is expired, refreshing...")
                 access_token = refresh_access_token(state, 'urn:my_service', 'read write')
@@ -157,7 +133,6 @@ def index():
 def logout():
     resp = make_response('')    # make empty response that clears cookies
     resp.set_cookie('auth', '', expires=0)
-    resp.set_cookie('token', '', expires=0)
     return resp, 204
 
 
@@ -166,25 +141,12 @@ def logged_out():
     return render_template('post_logout.html')
 
 
-def authenticated_session(token):
-    try:
-        if not token:
-            logger.info("Session cookie not found")
-            return None
-        claims = jwt.decode(str.encode(token), public_key, audience='https://localhost:5000', algorithms='RS256')
-        logger.info("Session cookie is valid")
-        return claims
-    except jwt.ExpiredSignatureError:
-        logger.info("Session cookie expired")
-        return None
-
-
 def authorize_request(client, scope):
     logger.info('Requesting token(s) with scope: ' + scope)
     state = str(uuid.uuid4())
     scopes[state] = scope
-    return redirect(request_url(AUTHORIZE_ENDPOINT, client_id=client_id,
-                                redirect_uri=redirect_uri, response_type='code', state=state, scope=scope,
+    return redirect(request_url(app.config.get('AUTHORIZE_ENDPOINT'), client_id=client['client_id'],
+                                redirect_uri=client['redirect_uri'], response_type='code', state=state, scope=scope,
                                 response_mode='form_post'))
 
 
@@ -223,14 +185,14 @@ def get_tokens(auth_code, state):
     redirect_url = 'https://localhost:5001/cb'
     headers = {}
     headers['Content-Type'] = "application/x-www-form-urlencoded"
-    headers['Authorization'] = authorization_header()
+    headers['Authorization'] = authorization_header(client)
 
     data = {"grant_type": "authorization_code",
             "code": auth_code,
             "redirect_uri": quote(redirect_url),
-            "client_id": client_id}
+            "client_id": client['client_id']}
 
-    response = requests.post(TOKEN_ENDPOINT, headers=headers, data=data, verify=False)
+    response = requests.post(app.config.get('TOKEN_ENDPOINT'), headers=headers, data=data, verify=False)
     if response.status_code == 200:
         access_token = response.json()["access_token"]
         logger.info('access_token: ' + access_token)
@@ -256,7 +218,7 @@ def refresh_access_token(subject, audience, scope):
 
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
-        'Authorization': authorization_header()
+        'Authorization': authorization_header(client)
     }
 
     data = {
@@ -264,7 +226,7 @@ def refresh_access_token(subject, audience, scope):
         "refresh_token": scope
     }
 
-    response = requests.post(TOKEN_ENDPOINT, headers=headers, data=data, verify=False)
+    response = requests.post(app.config.get('TOKEN_ENDPOINT'), headers=headers, data=data, verify=False)
     if response.status_code == 200:
         access_token = response.json()["access_token"]
         tokencache.add(subject, scope, access_token, 'access_token')
@@ -276,17 +238,51 @@ def refresh_access_token(subject, audience, scope):
     raise TokenError('Error refreshing token: ' + str(response))
 
 
-def authorization_header():
-    return 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode()).decode('utf-8')
+def authorization_header(client):
+    return 'Basic ' + base64.b64encode((client['client_id'] + ':' + client['client_secret']).encode()).decode('utf-8')
 
 
-def get_token_claims(token, audience):
+def get_token_claims(token, audience, public_key):
     logger.info(token)
     claims = jwt.decode(str.encode(token), public_key, audience=audience, algorithms='RS256')
     return claims
 
 
-def main():
+def main(config_path):
+    global logger
+    logger = init_logging(__name__)
+    global client
+
+    config = init_config(config_path)
+    app.config['JKWS_ENDPOINT'] = config['endpoints']['issuer'] + config['endpoints']['jwks']
+    app.config['TOKEN_ENDPOINT'] = config['endpoints']['issuer'] + config['endpoints']['token']
+    app.config['AUTHORIZE_ENDPOINT'] = config['endpoints']['issuer'] + config['endpoints']['authorize']
+    app.config['REGISTRATION_ENDPOINT'] = config['endpoints']['issuer'] + config['endpoints']['registration']
+    app.config['RESOURCE_ENDPOINT'] = config['endpoints']['resource_server'] + config['endpoints']['resource']
+    app.config['USERINFO_ENDPOINT'] = config['endpoints']['issuer'] + config['endpoints']['userinfo']
+    app.config['POST_LOGOUT_URI'] = config['endpoints']['webclient'] + config['endpoints']['post_logout']
+
+    # read client id and secret from environment
+    client_id = os.environ.get('CLIENT_ID')
+    if client_id:
+        client = {
+            'client_id': client_id,
+            'client_secret': os.environ['CLIENT_SECRET'],
+            'redirect_uri': os.environ['REDIRECT_URI']
+        }
+    else:
+        # if not set, register client and use it's id and secret
+        registered_client = register_client(config)
+        logger.info(registered_client)
+        client = {
+            'client_id': registered_client['client_id'],
+            'client_secret': registered_client['client_secret'],
+            'redirect_uri': registered_client['redirect_uris'][0]
+        }
+        logger.info(client)
+
+    logger.info(f'Registered client with client_id: {client_id}')
+
     app.run(host='0.0.0.0', port=5001, debug=app.config['TESTING'],
             ssl_context=('cert.pem', 'key.pem'))
 
